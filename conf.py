638f05250cd24d8de5de6554373b54ffd4305f5c
unknown <suayg3@gmail.com>
2015-02-15 23:13:38 -0700

    cr

--- conf.py
+++ conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'El Orgusu ve Dantel'
-copyright = u'2014, Celal Cakiroglu'
+copyright = u'2015, Sahika Cakiroglu'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
